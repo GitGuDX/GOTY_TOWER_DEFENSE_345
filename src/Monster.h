cb8dc0755454cab5d2d842691d10b5c5560f538e
Kyo young Lim <limkyouyou@gmail.com>
2025-02-13 20:54:51 -0500

    Add Monster class level member variable, getter and setter

--- src/Monster.h
+++ src/Monster.h
@@ -19,6 +19,26 @@ public:
         return m_stCurrentPathIndex;
     }
 
+    int GetHealth()
+    {
+        return m_iHealth;
+    }
+
+    float GetSpeed()
+    {
+        return m_fSpeed;
+    }
+
+    int GetLevel()
+    {
+        return m_iLevel;
+    }
+
+    void SetHealth(int newHealth)
+    {
+        m_iHealth = newHealth;
+    }
+
     void SetSpeed(float newSpeed)
     {
         m_fSpeed = newSpeed;
@@ -28,10 +48,17 @@ public:
     {
         m_stCurrentPathIndex = newIndex;
     }
+
+    void SetLevel(int newLevel)
+    {
+        m_iLevel = newLevel;
+    }
+
 private:
+    size_t m_stCurrentPathIndex;           // index of the monster's current path
     int m_iHealth;
     float m_fSpeed;
-    size_t m_stCurrentPathIndex;           // index of the monster's current path
+    int m_iLevel;
     /////// add monster type, levels, etc
 };
 
