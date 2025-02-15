7c163a1d8133ea56bff4818c4718d1e3666736d0
Victor Iliescu <58787321+GitGuDX@users.noreply.github.com>
2025-02-15 16:50:46 -0500

    added different tower types

--- src/Tower.cpp
+++ src/Tower.cpp
@@ -1,13 +1,27 @@
-
 #include "Tower.h"
 
-
-
-Tower::Tower() 
-    : m_fShootCooldown(0.5f)
-    , m_fMaxCooldown(1.0f)
-    , m_fRange(300.0f)
+Tower::Tower(TowerType type) 
+    : m_eType(type)
+    , m_fShootCooldown(0.0f)
+    , m_iHealth(100)
 {
+    switch(type) {
+        case TowerType::Basic:
+            m_fRange = 300.0f;
+            m_fMaxCooldown = 3.0f;
+            m_fDamage = 10.0f;
+            break;
+        case TowerType::Sniper:
+            m_fRange = 600.0f;
+            m_fMaxCooldown = 5.0f;
+            m_fDamage = 50.0f;
+            break;
+        case TowerType::Rapid:
+            m_fRange = 200.0f;
+            m_fMaxCooldown = 1.0f;
+            m_fDamage = 5.0f;
+            break;
+    }
 }
 
 Tower::~Tower()
--- src/Tower.h
+++ src/Tower.h
@@ -4,11 +4,16 @@
 #include "Entity.h"
 #include <iostream>
 
+enum class TowerType {
+    Basic,
+    Sniper,
+    Rapid
+};
 
 class Tower : public Entity
 {
 public:
-    Tower(); // Constructor defined inline
+    Tower(TowerType type = TowerType::Basic); // Default constructor takes a type
     ~Tower();
 
     void DebugPrint() const 
@@ -45,7 +50,10 @@ public:
     void ResetCooldown() { m_fShootCooldown = m_fMaxCooldown; }
     void UpdateCooldown(float deltaTime) { m_fShootCooldown -= deltaTime; }
 
+    TowerType GetType() const { return m_eType; }
+
 private:
+    TowerType m_eType;
     int m_iHealth;
     float m_fDamage;
     size_t m_stCurrentPathIndex;           // index of the Tower's current path
@@ -53,7 +61,5 @@ private:
     float m_fMaxCooldown;
     float m_fRange;
 };
-
-
 
 #endif
